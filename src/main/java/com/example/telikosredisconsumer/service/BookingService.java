663f24e235ef518f87f04c93d48c0ff04ae8d561
SivapragashC <sivapragash.c@maersk.com>
2023-01-11 11:13:10 +0530

    Autowiring the CacheService class

--- src/main/java/com/example/telikosredisconsumer/service/BookingService.java
+++ src/main/java/com/example/telikosredisconsumer/service/BookingService.java
@@ -2,7 +2,7 @@ package com.example.telikosredisconsumer.service;
 
 import com.example.telikosredisconsumer.entity.Booking;
 import com.example.telikosredisconsumer.repository.BookingRepository;
-import com.example.telikosredislibrary.service.CacheImpl;
+import com.example.telikosredislibrary.service.CacheService;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,7 @@ import reactor.core.publisher.Mono;
 public class BookingService {
 
     @Autowired
-    CacheImpl cacheImpl;
+    CacheService cacheService;
 
     @Autowired
     BookingRepository bookingRepository;
@@ -26,18 +26,18 @@ public class BookingService {
     private int entryTtl;
 
     public Mono<Booking> getBooking(String bookingId) {
-        return cacheImpl.get(bookingId)
+        return cacheService.get(bookingId)
                 .switchIfEmpty(getBookingById(bookingId).doOnNext(b -> {
-                    cacheImpl.put(b.getBookingId(), b).subscribe(e -> {
+                    cacheService.put(b.getBookingId(), b).subscribe(e -> {
                     });
                     log.info("Getting the key {} from DB", bookingId);
                 }));
     }
 
     public Mono<Booking> getBookingTTL(String bookingId) {
-        return cacheImpl.get(bookingId)
+        return cacheService.get(bookingId)
                 .switchIfEmpty(getBookingById(bookingId).doOnNext(b -> {
-                    cacheImpl.put(b.getBookingId(), b, entryTtl).subscribe(e -> {
+                    cacheService.put(b.getBookingId(), b, entryTtl).subscribe(e -> {
                     });
                     log.info("Getting the key {} from DB", bookingId);
                 }));
